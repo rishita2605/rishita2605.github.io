6dc62303c9e6bccad6a9441dedb76a62dc067b36
rishita2605 <64982040+rishita2605@users.noreply.github.com>
2022-11-06 01:40:55 +0530

    feat: responsive padding

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import Loader from "./components/loader/loader";
 import "./App.css";
-import HomePage from "./components/home-page/home-page";
+import HomePage from "./pages/home-page/home-page";
 
 function App() {
   const [loading, setLoading] = useState(false); // Set to false to enable loader
---
+++ src/pages/home-page/home-page.jsx
@@ -0,0 +1,15 @@
+import "../../assets/styles/home-page.css";
+// import { useState, useEffect } from "react";
+
+function HomePage() {
+  return (
+    <div className="main homepage" id="homepage">
+      <div className="grid">
+        <div className="grid__column"></div>
+        <div className="grid__column"></div>
+        <div className="grid__column"></div>
+      </div>
+    </div>
+  );
+}
+export default HomePage;
